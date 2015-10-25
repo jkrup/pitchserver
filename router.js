3a9a727b289288be0218687f82c496b4d8c2119a
Justin Krup <mazlix@Mazlix.local>
2015-10-25 02:51:22 -0700

    web working

--- router.js
+++ router.js
@@ -1,3 +1,12 @@
+Router.route('get', {
+  path: '/get',
+  where: 'server',
+  action: function () {
+    this.response.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    var resp = OculusData.find({},{sort: {timestamp: -1}, fields: {_id: 0}, limit: 1 }).fetch()[0];
+    this.response.end(JSON.stringify(resp));
+  }
+})
 Router.route('save', {
   path: '/save',
   where: 'server',
@@ -6,13 +15,14 @@ Router.route('save', {
       'w' : this.request.body.w,
       'x' : this.request.body.x,
       'y' : this.request.body.y,
-      'z' : this.request.body.z
+      'z' : this.request.body.z,
+      'timestamp' : new Date()
     };
 
     OculusData.insert(datum);
 
     this.response.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    this.response.end(JSON.stringify(resp));
+    this.response.end(JSON.stringify(datum));
   }
 });
